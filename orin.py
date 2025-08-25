0e587f964deabb721a0dce1b9ef069a7401d7621
Nick Cao <nickcao@nichi.co>
2025-08-25 11:08:07 -0400

    Add xavier

--- orin.py
+++ orin.py
@@ -1,31 +1,58 @@
 from bs4 import BeautifulSoup
+from more_itertools import consume
 import requests
 import pandas as pd
 from collections import OrderedDict
 
 
-def main():
-    r = requests.get(
-        "https://www.nvidia.com/en-us/autonomous-machines/embedded-systems/jetson-orin/"
-    )
+def url_to_df(
+    url,
+    offset,
+    name_offset,
+    table_id="jetson-prod-module-table",
+):
+    r = requests.get(url)
     r.raise_for_status()
     s = BeautifulSoup(r.text, features="html.parser")
-    table = s.find(id="jetson-prod-module-table")
+    table = s.find(id=table_id)
     data = OrderedDict()
-    for ir, row in enumerate(table.find_all("tr")):
+    rows = iter(table.find_all("tr"))
+    consume(rows, offset)
+    data["Name"] = [col.get_text() for col in next(rows).find_all("td")][name_offset:]
+    for row in rows:
         content = []
-        if ir == 0:
-            continue
-        if ir == 1:
-            label = "Name"
         for ic, col in enumerate(row.find_all("td")):
-            if ic == 0 and ir != 1:
-                label = col.get_text()
+            if ic == 0:
+                label = col.get_text().rstrip("*")
+                # match label:
+                #     case "Vision Accelerator" | "DL Accelerator":
+                #         label = "Accelerator"
+                #     case "Camera" | "CSI Camera":
+                #         label = "Camera"
             else:
                 content.extend(int(col.get("colspan", 1)) * [col.get_text()])
         data[label] = content
-    df = pd.DataFrame(data)
-    df.to_excel("orin.xlsx")
+    return pd.DataFrame(data)
+
+
+def main():
+    orin = url_to_df(
+        "https://www.nvidia.com/en-us/autonomous-machines/embedded-systems/jetson-orin/",
+        offset=1,
+        name_offset=0,
+    )
+    thor = url_to_df(
+        "https://www.nvidia.com/en-us/autonomous-machines/embedded-systems/jetson-thor/",
+        offset=0,
+        name_offset=1,
+    )
+    xavier = url_to_df(
+        "https://www.nvidia.com/en-us/autonomous-machines/embedded-systems/jetson-xavier-series/",
+        offset=1,
+        name_offset=0,
+        table_id="jetson-xavier-table",
+    )
+    pd.concat([orin, thor, xavier]).to_excel("jetson.xlsx")
 
 
 if __name__ == "__main__":
