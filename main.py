b63dd5421f3a281f35b69359c9dcc6a0c4714196
Nick Cao <nickcao@nichi.co>
2025-08-25 10:08:17 -0400

    prepare to analyze data with pandas

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ def main():
     )
     r.raise_for_status()
     s = BeautifulSoup(r.text, features="html.parser")
-    print(s.find(id="jetson-prod-module-table"))
+    table = s.find(id="jetson-prod-module-table")
+    for row in table.find_all("tr"):
+        print("------------------------------------------")
+        for col in row.find_all("td"):
+            print(col.get("colspan", 1))
+            print(col.get_text())
 
 
 if __name__ == "__main__":
